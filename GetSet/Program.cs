f12fc46a5822f3bcd42fd4ca1df0ff56bf20ff99
Thomas Vuille <tvuille@yahoo.fr>
2018-10-03 12:04:20 +0200

    Get Set

---
+++ GetSet/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rentrer le Prénom");
+            string leprenom = Console.ReadLine();
+            Console.WriteLine("Rentrer le Nom");
+            string lenom = Console.ReadLine();
+            Console.WriteLine("Rentrer l'age");
+            int lage = Int32.Parse(Console.ReadLine());
+
+            User user = new User();
+
+            user.nom = lenom;
+            user.prenom = leprenom;
+            user.age = lage;
+
+            Console.WriteLine($"prénom : {user.prenom}");
+            Console.WriteLine($"nom : {user.nom}");
+            Console.WriteLine($"age : {user.age}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GetSet/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSet
+{
+    class User
+    {
+        private string _nom;
+        private string _prenom;
+        private int _age;
+
+        public string nom
+        {
+            get => _nom;
+            set => _nom = value.ToUpper();
+        }
+        public string prenom
+        {
+            get { return _prenom; }
+            set { _prenom = value.ToLower(); }
+        }
+        public int age
+        {
+            get { return _age; }
+            set { _age = value-5; }
+        }
+
+       
+    }
+}
