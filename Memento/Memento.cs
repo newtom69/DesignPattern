a1abaf1e68733ba6b43fb35162b869ba1aecef98
Thomas Vuille <tvuille@yahoo.fr>
2018-10-03 17:13:23 +0200

    suite memento

---
+++ Memento/Memento.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Memento
+{
+    class Memento
+    {
+
+        protected string _etat;
+
+        public string etat
+        {
+            get { return _etat; }
+            set { _etat = value; }
+        }
+
+
+    }
+}
